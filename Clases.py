cd5e948d7194002b9a198c94209de7ea41822147
maanualonso <manuskating02@gmail.com>
2020-12-04 10:44:31 +0100

    Clases.py

---
+++ Clases.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  4 10:40:42 2020
+
+@author: maanualonso & edunavata
+"""
+class Lemming():
+    def __init__(self, pos_x_inicio: int, pos_y_inicio: int):
+        self.pos_x = pos_x_inicio
+        self.pos_y = pos_y_inicio
