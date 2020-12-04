95a3f786d670acf0d4a979fe3d595d2200e33693
maanualonso <manuskating02@gmail.com>
2020-12-04 14:17:50 +0100

    Assets

--- Clases.py
+++ Clases.py
@@ -1,10 +1,58 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Dec  4 10:40:42 2020
+Created on Fri Dec  4 13:24:01 2020
 
-@author: maanualonso & edunavata
+@author: CASA
 """
-class Lemming():
-    def __init__(self, pos_x_inicio: int, pos_y_inicio: int):
-        self.pos_x = pos_x_inicio
-        self.pos_y = pos_y_inicio
+import random as rnd
+import pyxel
+
+class Interfaz():
+    
+    nivel: int
+    salvados: int
+    muertos: int
+    vivos: int
+    escaleras: int
+    paraguas: int
+    bloqueadores: int
+    
+    def __init__(self):
+        self.matrizTerreno = []
+        self.filas = 14
+        self.columnas = 16
+        
+        for fila in range(self.filas):
+            auxlst = []
+            for columna in range(self.columnas):
+                auxlst.append(0)
+            self.matrizTerreno.append(auxlst)
+    '''
+    def rellenarTerreno(self):
+        self.plataformas = []
+        numeroPlataformas = 7
+            
+        for _ in range(numeroPlataformas):
+            casillas = rnd.randint(5, 10)
+                
+            plataforma = []
+            for _ in range(casillas):
+                plataforma.append(1)
+                    
+            self.plataformas.append(plataforma)
+     '''           
+    def rellenarTerrenoJuego(self):
+        numeroPlataformas = 7
+        for i in range(numeroPlataformas):
+            tamaño = rnd.randint(5, 10)
+            casilla_x = rnd.randint(0,11)
+            while casilla_x > 16 - tamaño:
+                tamaño = rnd.randint(5, 10)
+                casilla_x = rnd.randint(0,11)
+            for a in range(tamaño):
+                self.matrizTerreno[i*2 + 1][casilla_x + a] = 1
+        print(self.matrizTerreno)
+            
+a = Interfaz()
+a = a.rellenarTerrenoJuego()
+#print(a)
